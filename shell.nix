28307e4e2e2bd1438897cb4895f43fa2ed4d8ffd
Anonymous456x <jayantch255@gmail.com>
2026-08-27 20:41:14 +0530

    Create shell.nix for Python development environment

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python311
+    python311Packages.pip
+    python311Packages.telegram-bot
+    python311Packages.python-dotenv
+    python311Packages.aiohttp
+  ];
+  
+  shellHook = ''
+    echo "✅ Python environment ready!"
+    python --version
+  '';
+}
